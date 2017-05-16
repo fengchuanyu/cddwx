a4b13e40cf715c524b71ee8211c8ede637e588b3
fengchuanyu364 <fengchuanyu@163.com>
2017-05-16 17:28:45 +0800

    cddwx new

---
+++ js/base/global.js
@@ -0,0 +1,18 @@
+/*货币转换函数*/
+function formatMoney(number, places, symbol, thousand, decimal) {
+        number = number || 0;
+        places = !isNaN(places = Math.abs(places)) ? places : 2;
+        symbol = symbol !== undefined ? symbol : "$";
+        thousand = thousand || ",";
+        decimal = decimal || ".";
+        var negative = number < 0 ? "-" : "",
+            i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "",
+            j = (j = i.length) > 3 ? j % 3 : 0;
+        return symbol + negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
+    }
+/*调用方法
+formatMoney(54321); // $54,321
+formatMoney(12345, 0, "£ "); // £ 12,345
+formatMoney(12345, 2, "£ "); // £ 12,345.00
+formatMoney(12345.232, 2, "£ "); // £ 12,345.23
+*/
